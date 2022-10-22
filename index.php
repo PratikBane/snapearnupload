fb94b5f41013ac7a6be188e051d4f5aa4cd4b980
PratikBane <74564886+PratikBane@users.noreply.github.com>
2022-10-22 18:19:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
+<?php
+$image=$_POST["image"];
+if($image!=""){
+ $pic=date("Ymdhis").".jpg";
+ file_put_cntents("images/",base64_decode($image));
+ echo "upload done!";
+}
